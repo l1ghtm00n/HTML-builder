0ca90f09cc535136fe97f51d7add51b25873ac07
l1ghtm00n <lizardfrogdamn@gmail.com>
2022-11-02 17:03:00 +0300

    feat: made 04 copy directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, '/files-copy'), { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    fs.readdir(path.join(__dirname, '/files-copy'), (err, files) => {
+      if (err) throw err;
+      for (const file of files) {
+          fs.unlink(path.join(__dirname, `/files-copy/${file}`), (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+    fs.readdir(path.join(__dirname, '/files'), {withFileTypes: true}, (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          if(file.isFile()) {
+            fs.copyFile(path.join(__dirname, `/files/${file.name}`),
+                        path.join(__dirname, `/files-copy/${file.name}`),
+                        fs.constants.COPYFILE_FICLONE, ()=>{console.log(`File ${file.name} copied!`)});
+          };
+        })
+      }
+    });
+    console.log('Directory created successfully!');
+});
