de3049b565e2686f7c93868c364ac0b5488a3372
l1ghtm00n <lizardfrogdamn@gmail.com>
2022-11-04 12:11:54 +0300

    feat: full completed 06 build page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,10 +8,8 @@ const componentsDir = '/components';
 
 function clearFolder (dir) {
     fs.readdir(path.join(__dirname, dir), {withFileTypes: true}, (err, files) => {
-        if (err) throw err;
-        
-        for (const file of files) {
-            
+        if (err) throw err;        
+        for (const file of files) {            
           if(file.isFile()) {
             fs.unlink(path.join(__dirname, `${dir}/${file.name}`), (err) => {
               if (err) throw err;            
@@ -30,64 +28,49 @@ function clearFolder (dir) {
 }
 
 async function createFolder (...dir) {
-    let folders;
-    try {
-        folders = await fs.mkdir(path.join(__dirname, dir.join('')), { recursive: true });
-    } catch (error) {
-        console.log(err);
-    }    
+  let folders;
+  try {
+    folders = await fs.mkdir(path.join(__dirname, dir.join('')), { recursive: true });
+  } catch (error) {
+    console.log(err);
+  }
 }
 
 async function copyFile (dir, fileFrom, fileTo) {
-    let files;
-    try {
-        files = await fs.copyFile(path.join(__dirname, dir,`/${fileFrom}`),
-                                  path.join(__dirname, projectDir, dir,`/${fileTo}`),
-                                  fs.constants.COPYFILE_FICLONE);
-    } catch (err) {
-        console.log(err);
-    }
+  let files;
+  try {
+    files = await fs.copyFile(path.join(__dirname, dir,`/${fileFrom}`),
+                              path.join(__dirname, projectDir, dir,`/${fileTo}`),
+                              fs.constants.COPYFILE_FICLONE);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function copyFolder(dir) {
-    fsHandler.readdir(path.join(__dirname, dir), {withFileTypes: true}, (err, files) => {
-        if (err)
-          console.log(err);
-        else {
-          files.forEach(file => {
-            if(file.isFile()) {
-              copyFile(dir, file.name, file.name);
-            } else {              
-              let newDir =`${dir}/${file.name}`;
-              createFolder(projectDir, newDir);
-              copyFolder(newDir);
-            };
-          })
-        }
-      });
+async function copyFolder(dir) {
+  const files = await fs.readdir(path.join(__dirname, dir), {withFileTypes: true});
+  for (const file of files) {
+    if(file.isFile()) {
+      await copyFile(dir, file.name, file.name);
+    } else {              
+      let newDir =`${dir}/${file.name}`;
+      await createFolder(projectDir, newDir);
+      await copyFolder(newDir);
+    };
+  }
 }
 
 async function createBundle(dir) {
-    const writeStreamBundle = fsHandler.createWriteStream(path.join(__dirname, projectDir, 'style.css'), 'utf8');
-    let stylePath = path.join(__dirname, dir);
-    fs.readdir(stylePath, {withFileTypes: true}, (err, files) => {
-        if (err)
-          console.log(err);
-        else {
-          files.forEach(file => {
-            let el = path.parse(file.name);
-            if (el.ext==='.css') {
-                let readStream = fs.createReadStream(path.join(stylePath, file.name), {encoding: 'utf8'});
-                readStream.on('readable', function(){
-                  let data = readStream.read();
-                  if (data) {
-                    writeStreamBundle.write(`${data}\n`);
-                  }
-                });
-            }
-          })
-        }
-    });
+  const writeStreamBundle = fsHandler.createWriteStream(path.join(__dirname, projectDir, 'style.css'), 'utf8');
+  let stylePath = path.join(__dirname, dir);
+  const files = await fs.readdir(stylePath, {withFileTypes: true});
+  for (const file of files) {
+    let el = path.parse(file.name);
+    if (el.ext==='.css') {
+      let contents = await fs.readFile(path.join(stylePath, file.name), 'utf8');
+      writeStreamBundle.write(`${contents}\n`);
+    }
+  }
 }
 
 function isComponent(file, component) {
@@ -103,53 +86,50 @@ function isComponent(file, component) {
 
 async function insertComponents (dir, textHTML) {
   const files = await fs.readdir(dir, {withFileTypes: true});
-  let i = 0;
   for (const file of files) {
-      let el = path.parse(file.name);
-      let indexComponent = isComponent(textHTML, `{{${el.name}}}`);
-      if (el.ext==='.html' && indexComponent!==-1) {
-          let contents = await fs.readFile(path.join(dir, file.name), 'utf8');
-          // contents +='\n';
-          // console.log(contents);
-          textHTML[indexComponent+1] = `\n${textHTML[indexComponent+1]}`;
-          textHTML.splice.apply(textHTML,[indexComponent,1].concat(contents.split('\n').map((line) => `    ${line}`)));
-      }
+    let el = path.parse(file.name);
+    let indexComponent = isComponent(textHTML, `{{${el.name}}}`);
+    if (el.ext==='.html' && indexComponent!==-1) {
+      let contents = await fs.readFile(path.join(dir, file.name), 'utf8');
+      let nextTag = textHTML[indexComponent+1];
+      // let prevTag = textHTML[indexComponent-1];
+      let tab = '    ';
+      textHTML[indexComponent+1] = `\n${nextTag}`;
+      textHTML.splice.apply(textHTML,[indexComponent,1].concat(contents.split('\n').map((line) => `${tab}${line}`)));
+    }
   }
   return textHTML;
-
 }
 
 async function createHTML(dir) {
-    let indexHTML;
-    let indexPath = path.join(__dirname, projectDir, 'index.html');
-    try{
-        indexHTML = await fs.open(indexPath);
-        let componentPath = path.join(__dirname, dir);
-        const line = await fs.readFile(indexPath, 'utf8');
-        let newData = await insertComponents(componentPath, line.split('\n'));
-        fs.writeFile(indexPath, newData);
-    } finally {
-        await indexHTML?.close();
-    }
-
+  let indexHTML;
+  let indexPath = path.join(__dirname, projectDir, 'index.html');
+  try{
+    indexHTML = await fs.open(indexPath);
+    let componentPath = path.join(__dirname, dir);
+    const line = await fs.readFile(indexPath, 'utf8');
+    let newData = await insertComponents(componentPath, line.split('\n'));
+    fs.writeFile(indexPath, newData);
+  } finally {
+    await indexHTML?.close();
+  }
 }
 
 
 async function buildPage() {
-    // clearFolder(projectDir);
-    try {
-        let projectFolder = await createFolder(projectDir);
-        // let createAssets = await createFolder(projectDir,assetsDir);
-        // let copyAssets = await copyFolder(assetsDir);
-        // let createCSS = await createBundle(stylesDir);
-        let copyIndex = await copyFile('','template.html','index.html');
-        let createIndex = await createHTML(componentsDir);
-    } catch(err) {
-        console.log(err);
-    }
-
+  // clearFolder(projectDir);
+  try {
+    await createFolder(projectDir);
+    await createFolder(projectDir,assetsDir);
+    await copyFolder(assetsDir);
+    await createBundle(stylesDir);
+    await copyFile('','template.html','index.html');
+    await createHTML(componentsDir);
+  } catch(err) {
+    console.log(err);
+  }
 }
-    
+
 buildPage();
 
 
